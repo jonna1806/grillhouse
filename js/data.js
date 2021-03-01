f7cf505ed684eaa3d6a01a397032f8125e2ed9b1
Jonnathan Arias <jonnathan1806@gmail.com>
2021-02-28 19:02:36 -0500

    creación de data.js

--- js/data.js
+++ js/data.js
@@ -1,8 +1,132 @@
 const productosData = [
-    //Hamburguesas
+//Hamburguesas
     {
         name: `Miss Piggy`,
-        descripcion: ` Combinación de nuestros mejores sabores, con un toque secreto del chef`,
+        descripcion: `Pan artesanal, vegetales frescos y 250 gr. de cerdo con tocineta y salsa de la casa`,
         precio: 27.000,
+    },
+    {
+        name: `Classic`,
+        descripcion: `250 gr. de res, queso cheddar, tomate y lechuga fresca y aros de cebolla caramelizada`,
+        precio: 22.000,
+    },
+    {
+        name: `Bacon King`,
+        descripcion: `250 gr. de res con triple porción de tocineta, queso cheddar y salsa de la casa`,
+        precio: 25.000,
+    },
+    {
+        name: `Chesse Rain`,
+        descripcion: `150gr. de cerdo, tocineta, tomate y triple porción de queso cheddar y mozzarella`,
+        precio: 24.000,
+    },
+    {
+        name: `XXL`,
+        descripcion: `150 gr. de res y 150 gr. de cerdo, aros de cebolla y quedo cheddar doble ración de tocineta`,
+        precio: 29.000,
+    },
+ //Hotdogs
+    {
+        name: `Alemán`,
+        descripcion: `Pan artesanal, salchicha alemana, queso cheddar, pesto y salsa tártara`,
+        precio: 25.000,
+    },
+    {
+        name: `Todero`,
+        descripcion: `Salchicha ranchera ahumada con trozos de tocineta, queso mozzarella y perejil`,
+        precio: 22.000,
+    },
+    {
+        name: `Casero`,
+        descripcion: `Pan casero con salchicha ranchera, trozos de cebolla, ensalada y mostaza`,
+        precio: 20.000,
+    },
+    {
+        name: `Veggie`,
+        descripcion: `Champiñones salteados, albahaca, ensalada, trozos de cebolla y mostaza`,
+        precio: 18.000,
+    },
+    {
+        name: `Sencillito`,
+        descripcion: `Pan casero, salchicha, aros de cebolla salteados y salsa ketchup y mostaza`,
+        precio: 17.000,
+    },
+//Pizzas
+    {
+        name: `Consentida`,
+        descripcion: `Crocante maza, jamón serrano, rúgula, trozos de manzana y queso mozzarella`,
+        precio: 29.000,
+    },
+    {
+        name: `Manzana`,
+        descripcion: `Crocante maza, jamón serrano, rúgula, trozos de manzana y queso mozzarella`,
+        precio: 22.000,
+    },
+    {
+        name: `Pepperoni`,
+        descripcion: `Maza con bordes de queso, aros de pepperoni, albahaca y queso cheddar`,
+        precio: 23.000,
+    },
+    {
+        name: `4 Quesos`,
+        descripcion: `Salsa napolitana y encima queso tipo mozzarella, gorgonzola, fontina y parmesano`,
+        precio: 22.000,
+    },
+    {
+        name: `Aceituna`,
+        descripcion: `Maza con queso mozzarella y sobre una mezcla de aceitunas con tocineta y jamón`,
+        precio: 21.000,
+    },
+//Papas
+    {
+        name: `California`,
+        descripcion: `Nuestra más tradicional receta, con salsa de la casa y un toque de perejil`,
+        precio: 20.000,
+    },
+    {
+        name: `Paisa`,
+        descripcion: `Papas en fósforos con tiras de carne y trozos de salchicha en salsa chipotle`,
+        precio: 21.000,
+    },
+    {
+        name: `Mexicanas`,
+        descripcion: `Crocantes papas rociadas con chile en polvo y salsa de la casa con un toque de ají`,
+        precio: 18.000,
+    },
+    {
+        name: `Tradicionales`,
+        descripcion: `Papa criolla, bañadas en nuestra fórmula secreta de especias, no te arrepentirás`,
+        precio: 17.000,
+    },
+    {
+        name: `Pacono`,
+        descripcion: `Papas en fosforitos con queso crema y chicharrón, justo para calmar el antojo`,
+        precio: 17.000,
+    },
+//Malteadas
+    {
+        name: `Afrodita`,
+        descripcion: `Deliciosa mezcla de frutos rojos, chantilly y trozos de frezas picadas`,
+        precio: 15.000,
+    },
+    {
+        name: `Blueberry`,
+        descripcion: `Arándanos en pequeños trozos, mortiño fresco y helado de veteado de mora`,
+        precio: 14.000,
+    },
+    {
+        name: `Mocaccino`,
+        descripcion: `Exquisito café con un toque de Baileys, chantilly y mocaccino espolvoreado`,
+        precio: 13.000,
+    },
+    {
+        name: `Cereza`,
+        descripcion: `Helado de vainilla con jugosas cerezas y trozos de mora picadas, todo un delite`,
+        precio: 14.000,
+    },
+    {
+        name: `Caramelo`,
+        descripcion: `Delicioso helado de veteado de caramelo, chantilly y trozos de caramelo picados`,
+        precio: 11.000,
     },
 ]
