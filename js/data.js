0e73d4556c14ba1e3d7661c853e3abcd8f088460
Jonnathan Arias <jonnathan1806@gmail.com>
2021-03-07 18:54:05 -0500

    ensayando evento clic en los botones

--- js/data.js
+++ js/data.js
@@ -1,121 +1,121 @@
 const productosData = [
 //Hamburguesas
-    {
+    {   
         name: `Miss Piggy`,
         descripcion: `Pan artesanal, vegetales frescos y 250 gr. de cerdo con tocineta y salsa de la casa`,
         img: '..img/burger_miss_piggy.jpeg',
-        precio: 27.000
+        precio: 27000
     },
     {
         name: `Classic`,
         descripcion: `250 gr. de res, queso cheddar, tomate y lechuga fresca y aros de cebolla caramelizada`,
         img: '../img/burger_classic.jpeg',
-        precio: 22.000
+        precio: 22000
     },
     {
         name: `Bacon King`,
         descripcion: `250 gr. de res con triple porción de tocineta, queso cheddar y salsa de la casa`,
         img: '../img/burger_bacon_king.jpeg',
-        precio: 25.000
+        precio: 25000
     },
     {
         name: `Chesse Rain`,
         descripcion: `150gr. de cerdo, tocineta, tomate y triple porción de queso cheddar y mozzarella`,
         img: '../img/burger_chesse_rain.jpeg',
-        precio: 24.000
+        precio: 24000
     },
     {
         name: `XXL`,
         descripcion: `150 gr. de res y 150 gr. de cerdo, aros de cebolla y quedo cheddar doble ración de tocineta`,
         img: '../img/burger_xxl.jpeg',
-        precio: 29.000
+        precio: 29000
     },
  //Hotdogs
     {
         name: `Alemán`,
         descripcion: `Pan artesanal, salchicha alemana, queso cheddar, pesto y salsa tártara`,
         img: '../img/hotdog_aleman',
-        precio: 25.000
+        precio: 25000
     },
     {
         name: `Todero`,
         descripcion: `Salchicha ranchera ahumada con trozos de tocineta, queso mozzarella y perejil`,
         img: '../img/hotdog_todero.jpg',
-        precio: 22.000
+        precio: 22000
     },
     {
         name: `Casero`,
         descripcion: `Pan casero con salchicha ranchera, trozos de cebolla, ensalada y mostaza`,
         img: '../img/hotdog_casero.jpeg',
-        precio: 20.000
+        precio: 20000
     },
     {
         name: `Veggie`,
         descripcion: `Champiñones salteados, albahaca, ensalada, trozos de cebolla y mostaza`,
         img: '../img/hotdog_veggie.jpeg',
-        precio: 18.000
+        precio: 18000
     },
     {
         name: `Sencillito`,
         descripcion: `Pan casero, salchicha, aros de cebolla salteados y salsa ketchup y mostaza`,
         img: '../img/hotdog_sencillito.jpeg',
-        precio: 17.000
+        precio: 17000
     },
 //Pizzas
     {
         name: `Consentida`,
         descripcion: `Crocante maza, jamón serrano, rúgula, trozos de manzana y queso mozzarella`,
         img: '../img/pizza_consentida.jpeg',
-        precio: 29.000
+        precio: 29000
     },
     {
         name: `Manzana`,
         descripcion: `Crocante maza, jamón serrano, rúgula, trozos de manzana y queso mozzarella`,
         img: '../img/pizza_manzana.jpeg',
-        precio: 22.000
+        precio: 22000
     },
     {
         name: `Pepperoni`,
         descripcion: `Maza con bordes de queso, aros de pepperoni, albahaca y queso cheddar`,
         img: '../img/pizza_pepperoni.jpeg',
-        precio: 23.000
+        precio: 23000
     },
     {
         name: `4 Quesos`,
         descripcion: `Salsa napolitana y encima queso tipo mozzarella, gorgonzola, fontina y parmesano`,
         img: '../img/pizza_4quesos.jpeg',
-        precio: 22.000
+        precio: 22000
     },
     {
         name: `Aceituna`,
         descripcion: `Maza con queso mozzarella y sobre una mezcla de aceitunas con tocineta y jamón`,
         img: '../img/pizza_aceitunas.jpeg',
-        precio: 21.000
+        precio: 21000
     },
 //Papas
     {
         name: `California`,
         descripcion: `Nuestra más tradicional receta, con salsa de la casa y un toque de perejil`,
         img: '../img/fries_california.jpeg',
-        precio: 20.000
+        precio: 20000
     },
     {
         name: `Paisa`,
         descripcion: `Papas en fósforos con tiras de carne y trozos de salchicha en salsa chipotle`,
         img: '../img/fries_paisa.jpeg',
-        precio: 21.000
+        precio: 21000
     },
     {
         name: `Mexicanas`,
         descripcion: `Crocantes papas rociadas con chile en polvo y salsa de la casa con un toque de ají`,
         img: '../img/fries_mexicana.jpeg',
-        precio: 18.000
+        precio: 18000
     },
     {
         name: `Tradicionales`,
         descripcion: `Papa criolla, bañadas en nuestra fórmula secreta de especias, no te arrepentirás`,
         img: '../img/fries_tradicionales.jpeg',
-        precio: 17.000
+        precio: 17000
     },
     {
         name: `Pacono`,
@@ -128,30 +128,30 @@ const productosData = [
         name: `Afrodita`,
         descripcion: `Deliciosa mezcla de frutos rojos, chantilly y trozos de frezas picadas`,
         img: '../img/milkshake_afrodita.jpeg',
-        precio: 15.000
+        precio: 15000
     },
     {
         name: `Blueberry`,
         descripcion: `Arándanos en pequeños trozos, mortiño fresco y helado de veteado de mora`,
         img: '../img/milkshake_blueberry.jpeg',
-        precio: 14.000
+        precio: 14000
     },
     {
         name: `Mocaccino`,
         descripcion: `Exquisito café con un toque de Baileys, chantilly y mocaccino espolvoreado`,
         img: '../img/milkshake_mocaccino.jpeg',
-        precio: 13.000
+        precio: 13000
     },
     {
         name: `Cereza`,
         descripcion: `Helado de vainilla con jugosas cerezas y trozos de mora picadas, todo un delite`,
         img: '../img/milkshake_cereza.jpeg',
-        precio: 14.000
+        precio: 14000
     },
     {
         name: `Caramelo`,
         descripcion: `Delicioso helado de veteado de caramelo, chantilly y trozos de caramelo picados`,
         img: '../img/milkshake_caramelo.jpeg',
-        precio: 11.000
+        precio: 11000
     },
 ]
--- js/main.js
+++ js/main.js
@@ -2,51 +2,61 @@
 const carroCompra = [];
 const data = productosData;
 
-let nombre = 0
 
 /*
-function pedirNombre() {
-    nombre = prompt ("Hola, bienvenido a Grill House, nos gustaría saber tu nombre");
-}
-pedirNombre();
-function solicitarPedido() {
-    pedido = prompt ("¡A lo que vinimos " + nombre + "! \n¿Qué te gustaría conocer de nuestro menú? \nHamburguesas > 1 \nHotdogs > 2 \nPizzas > 3 \nPapas > 4 \nMalteadas > 5");
-}
-solicitarPedido();
-
-
 function quieroBurger () {
-    var nuevoDiv = document.createElement("div");
-    var estoQuiero = document.createTextNode("ESTO ES LO QUE QUIERO");
+    let nuevoDiv = document.createElement("div");
+    let estoQuiero = document.createTextNode("ESTO ES LO QUE QUIERO");
     nuevoDiv.appendChild(estoQuiero);
 
-    var actualDiv = document.getElementById("burgers");
+    let actualDiv = document.getElementById("burgers");
     document.body.insertBefore(nuevoDiv, actualDiv);
 }
-
-if(pedido == 1) {
-    quieroBurger()
-} else if(pedido == 2){
-    alert("Nuestros hotdogs")
-} else if(pedido == 3){
-    alert("Nuestras pizzas")
- } else if(pedido == 4){
-    alert("Nuestras papas")
-} else if(pedido == 5){
-    alert("Nuestras malteadas")
-} else{
-    alert("No te gustó nada :C")
-}
+quieroBurger();
 */
 
-//Eventos
-let boton = document.getElementById("btnPrueba")
-boton.onclick = () => {
-    alert("El btn funciona");
+//Declaración de las clases
+class Burgers{
+    constructor(data){
+        this.name        = data.name;
+        this.descripcion = data.descripcion;
+        this.precio      = data.precio;
+    }
 }
 
-localStorage.setItem('Carrito', data.length);
+let listaBurgers = [];
+listaBurgers.push(new Burgers(data[0]));
+listaBurgers.push(new Burgers(data[1]));
+listaBurgers.push(new Burgers(data[2]));
+listaBurgers.push(new Burgers(data[3]));
+listaBurgers.push(new Burgers(data[4]));
+
+//Eventos de botón de compra
+var cantidadPedido = 0;
+
+let botones = document.getElementsByClassName("btn-buy");
+for (let boton of botones){
+    boton.onclick = () => {
+    cantidadPedido = prompt(`No te arrepentirás \n¿qué cantidad desesas de este producto?`)
     
+    function quieroBurger () {
+        var nuevoDiv = document.createElement("div");
+        var estoQuiero = document.createTextNode(`Quiero ${cantidadPedido} de hamburguesas`);
+        nuevoDiv.appendChild(estoQuiero);
+        var actualDiv = document.getElementById("burgers");
+        document.body.insertBefore(nuevoDiv, actualDiv);
+    }
+    quieroBurger();
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
