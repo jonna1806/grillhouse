3706be080f3f9f6f692f5e7448df5b73374b090f
Jonnathan Arias <jonnathan1806@gmail.com>
2021-04-05 23:53:31 -0500

    Agregando Tota precio al carrito

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-
 const buyCar = [];
-const data = productosData;
 let totalPrice = 0;
-const prefijoRemove = "productoID";
+const data = productosData;
 
 class ProductsByCar{
     constructor(product){
@@ -12,23 +10,12 @@ class ProductsByCar{
     }   
 }
 
-function priceSummary(price){    
-    totalPrice = parseFloat(totalPrice) + parseFloat(price)
-    return totalPrice;
-}
-
-function priceRest(price){    
-    console.log("Soy resta",price);
-    totalPrice = parseFloat(totalPrice) - parseFloat(price)
-    return totalPrice;
-}
-
 //Se recorre el Array de productos
 for (let productos of data){
     generateProducts(productos)
 }
 
-//Generar productos al sitio web
+//Generar productos
 function generateProducts(productos){
     let padres = document.getElementsByClassName("products")
     for (let padre of padres) {
@@ -45,14 +32,15 @@ function generateProducts(productos){
     }
     document.getElementById(productos.id).onclick = addCar;
 
-    //Efecto bontón AGREGAR
+    //Efecto bontón "Agregar"
     $(".btn-buy").mouseover(function() { 
         $(this).text("Agregar")
     });
 
+    //Efecto bontón Retorna a su precio
     $(".btn-buy").mouseout( function() { 
         $(this).text(`${productos.price}`)
-    });
+    });    
 }
 
 let items = document.getElementById("shopList");
@@ -61,7 +49,6 @@ iconShop.addEventListener("click", showItemsCar);
 
 //Mostrar productos al carrito de compra
 function showItemsCar(){
-    console.log("buy car", buyCar);
     items.innerHTML = "";
     if (buyCar.length > 0){        
         buyCar.map((element) => {
@@ -73,130 +60,48 @@ function showItemsCar(){
             document.getElementById(element.id).onclick = deleteCar;
         }) 
     } else {
+        //Mensaje de carrito vacio
         let containerList           = document.createElement("div");
             containerList.innerHTML = `<p>No hay elementos aún</p>` 
             items.appendChild(containerList);
     }
+    //Total de precios de productos en el carrito
+    let totalPriceDiv       = document.createElement("div");
+    totalPriceDiv.innerHTML = `<p>Precio total: <strong>${totalPrice}</strong></p> `
+    items.appendChild(totalPriceDiv);
 }
 
-//Añadir productos al carrito de compra
+// Función añadir productos al carrito de compra
 function addCar(event){
     let index = parseInt(event.target.id) - 1
     buyCar.push(new ProductsByCar(data[index]));
     priceSummary(data[index].price);
-    console.log(totalPrice)
 }
 
+// Función eliminar productos del carrito de compra
 function deleteCar(event){
-    for( let nuevoBuyCar in buyCar){
-        
+    for( let nuevoBuyCar in buyCar){        
         let indice = buyCar[nuevoBuyCar].id;
-        if (indice == event.target.id){
-            console.log("Soy indice", indice)
-            let index = nuevoBuyCar;
-            console.log("Soy index", index);
-            console.log("Soy total price antes de", totalPrice);
+        if (indice == event.target.id){            
+            let index = nuevoBuyCar;            
             priceRest(buyCar[index].price)
-            buyCar.splice(index, 1);
-            console.log("Soy total price después de", totalPrice);
+            buyCar.splice(index, 1);            
         }
     }
     showItemsCar()
 }
 
-/*
-var btnAll = document.getElementById("all").onclick = filterAll
-var btnAll = document.getElementById("burgers").onclick = filterBurgers
-var btnAll = document.getElementById("hotDog").onclick = filterHotDog
-var btnAll = document.getElementById("pizzas").onclick = filterHotDog
-var btnAll = document.getElementById("fries").onclick = filterFries
-var btnAll = document.getElementById("milkshakes").onclick = filterMilkshakes
-*/
-
-
-
-//Eventos de botón de compra
-var cantidadPedido = 0;
-
-//Ensayando con JQUERY
-/*
-$(".btn-buy").click(() => {
-    cantidadPedido = prompt(`No te arrepentirás \n¿qué cantidad desesas de este producto?`);
-    $(".btn-buy").prepend(`<p>Quiero ${cantidadPedido}</p>`)
-})
-
-
-//Ensayando con Efectos y animaciones
-$("#burgerMissPiggy").click( () => { 
-    $("#burgerMissPiggy").fadeOut(2000, () => {
-        $("#burgerMissPiggy").fadeIn("2000");
-    });   
-});
-
-//Aparece y desaparece los nombres de los productos
-$(".btn-buy").click(() => {
-    $(".name").toggle("fast");
-})
-
- 
-
-//Almacenar datos en el localStorage
-const productoPedido = localStorage.setItem("productosPedidos", JSON.parse(cantidadPedido));
-
-const guardarProductos = (a, b) => {localStorage.setItem(a, b)};
-
-for (const productos of data){
-    guardarProductos("listaProductos", JSON.stringify(data));
+// Función de sumar
+function priceSummary(price){    
+    totalPrice = parseFloat(totalPrice) + parseFloat(price)
+    return totalPrice;
 }
 
-//__________________________
-
-$( document) .ready(function(){
-    console.log('El DOM está listo');
-})
-
-window.addEventListener('load', function(){
-    console.log('Ventana cargada');
-})
-
-// Probando AJAX con Jquery
-const URLGET = "https://jsonplaceholder.typicode.com/posts"
-
-$("#pruebaAjax").prepend('<button id="btnPrueba">Desplegar información</button>')
-$("#pruebaAjax").prepend('<button id="btnEnviar">Enviar Información</button>')
-
-$("#btnPrueba").click(() => {
-    $.get(URLGET, function (respuesta, estado){
-        if(estado === "success"){
-            let misDatos = respuesta;
-            for (const dato of misDatos){
-                $("#pruebaAjax").prepend(`<div>
-                                            <p><strong>${dato.title}</strong><p>
-                                            <p> ${dato.body}</p>                                        
-                                          </div>`);
-                                          
-            }
-        }
-    })    
-})
-
-$("#btnEnviar").click(() => {
-    $.post(URLGET, data[0].name, (datos, estado) => {
-        if(estado === "success"){
-            alert ("envio exitoso");
-        }
-    })
-})
-*/
-
-
-
-
-
-
-
-
-
+// Función de restar
+function priceRest(price){    
+    totalPrice = parseFloat(totalPrice) - parseFloat(price)
+    return totalPrice;
+}
 
 
 
